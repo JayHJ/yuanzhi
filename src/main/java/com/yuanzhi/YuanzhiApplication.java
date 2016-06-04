98817709b62b148a1a698828a9ab81738ee02fcb
Jay <hejian811@gmail.com>
2016-06-04 20:09:44 +0800

    init commit

---
+++ src/main/java/com/yuanzhi/YuanzhiApplication.java
@@ -0,0 +1,12 @@
+package com.yuanzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuanzhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuanzhiApplication.class, args);
+	}
+}
